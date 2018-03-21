ecb0fabc8c1a111748fa53bd70bc4c1f326a2b18
signupromain <romain@machaharson.com>
2018-03-21 12:10:43 +0100

    hello try

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
 session_start();
+
+// appel du fichier de configuration
+require_once "mysqliconfig.php";
+
+//connexion à la DB
+require_once "m/connectDBModel.php";
+
+if(isset($_SESSION['myKey'])){
+    require_once "c/AdminController.php";
+}else{
+    require_once "c/PublicController.php";
+}
